4cc41f0f5ae5c875f05493a7ffe6bb21187f259a
mainaenock <mainaenock10@gmail.com>
2024-01-18 03:07:10 -0500

    upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    sent = ""
+    for i in str:
+        c = ord(i)
+        if ord('a') <= c <= ord('z'):
+            sent += (chr(c - 32))
+        else:
+            sent += (chr(c))
+    print(sent)
