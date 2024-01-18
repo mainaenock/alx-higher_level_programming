de93eff0a83f52db4c633fdb37d660dcec4a5b16
mainaenock <mainaenock10@gmail.com>
2024-01-18 12:46:46 -0500

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
         for i, arg in enumerate(argus, start=1):
             print("{}: {}".format(i,arg))
     else:
-            print("0 arguments.")
+        print("{}".format('0 arguments.'))
