f85ee4b7bf8d82b8f90da33f82b7f96d33dde437
mainaenock <mainaenock10@gmail.com>
2024-01-18 19:06:29 -0800

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
+import  hidden_4
 if __name__ == "__main__":
-    print("{}".format(hidden_4))
+    names = dir(hidden_4)
+    for i in names:
+        if i[0] != '_':
+            print(i)
