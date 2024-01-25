cd36bce14459a365e16260711336a8c8673ca88f
mainaenock <mainaenock10@gmail.com>
2024-01-25 11:03:33 -0500

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for elm in row:
-            print("{}".format(elm), end=" ")
+            print("{:d}".format(elm), end=" ")
         print()
