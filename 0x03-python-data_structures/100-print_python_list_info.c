62a1fac335e680fcb324a0dee000058c550da00f
mainaenock <mainaenock10@gmail.com>
2024-03-11 11:51:28 -0400

    readme file

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,9 +1,17 @@
-#include <stdio.h>
 #include <Python.h>
-
-oid print_python_list_info(PyObject *p);
 
 void print_python_list_info(PyObject *p)
 {
-	printf("Lists are amazing in python")
+    int size = PyList_Size(p);
+    int i;
+    PyObject *item;
+
+    printf("[*] Size of the Python List = %d\n", size);
+    printf("[*] Allocated = %d\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; i++) {
+        item = PyList_GetItem(p, i);
+        printf("Element %d: %s\n", i, Py_TYPE(item)->tp_name);
+    }
 }
+
