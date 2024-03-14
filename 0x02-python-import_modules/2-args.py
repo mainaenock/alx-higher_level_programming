4583a4f7b9a9a4a8da54fe10ee10fea4e3385c9f
mainaenock <mainaenock10@gmail.com>
2024-03-14 12:43:16 -0400

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     if a == 0:
         print("{}".format("0 arguments."))
     elif a ==1:
-        print("{} argument:\n{}: {}".format(1, 1, argv[1]))
+        print("{}\n{}: {}".format("1 argument:", 1, argv[1]))
     else:
-        print("{} arguments:".format(a))
+        print("{} {}".format(a,"arguments:"))
         for i in argv[1:]:
             print("{}: {}".format(count, i))
             count += 1
