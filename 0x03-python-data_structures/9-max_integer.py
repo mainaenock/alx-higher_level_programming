d4400d6ca33e420822bc12c73561bf9b5cb19d80
mainaenock <mainaenock10@gmail.com>
2024-03-19 22:48:41 -0400

    max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    maxx = my_list[0]
+    for i in my_list[1:]:
+        if i > maxx:
+            maxx = i
+    return maxx
