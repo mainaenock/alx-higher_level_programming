59e009d193da26265eac301dd1ce6350d5ffdb6e
mainaenock <mainaenock10@gmail.com>
2024-04-03 13:04:25 -0400

    setter

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+""" Define a class"""
+
+class Square:
+
+    """ __initialization__"""
+
+    def __init__(self, size=0):
+
+        """ init"""
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """ getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter"""
+        if not isinstance(value, int):
+            raise TypeError("size must be integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    def area(self):
+        """area"""
+        return self.__size * self.__size
+    def my_print(self):
+        for _ in range(self.__size):
+            print("{}".format("#" * self.__size))
