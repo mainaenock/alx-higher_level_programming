ac6b54f366b0b148955f749265b813f4e6d8be3a
mainaenock <mainaenock10@gmail.com>
2024-04-22 11:13:24 -0400

    test 1

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module divides matrix by a number div
+the div must be either an int or a float
+"""
+def matrix_divided(matrix, div):
+    len_matrix = len(matrix[0])
+    for row in matrix:
+        if not isinstance(row, (list)) and not (isinstance(num, (int, float)) for num in row):
+            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+    for row in matrix[1:]:
+        if len(row) != len_matrix:
+            raise TypeError('Each row of the matrix must have the same size')
+    if not isinstance(div, (int, float)):
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    result = []
+    for row in matrix:
+        results = [round((num / div), 2) for num in row]
+        result.append(results)
+    return result
