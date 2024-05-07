0271011cfb1b4833dc7e44b4e274b2697e1e50d5
mainaenock <mainaenock10@gmail.com>
2024-05-07 11:27:35 -0400

    inheritance 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+class declaration
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    checks the data type
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
