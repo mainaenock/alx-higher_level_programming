d3e3c7ba3b4278dc5f1ffd30ef370099f75eb9b9
mainaenock <mainaenock10@gmail.com>
2024-05-07 11:32:33 -0400

    inheritance 2

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Define function
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    checks if isinstance
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
