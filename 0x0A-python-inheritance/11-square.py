9b4646e3cb7bc6f04787631f782f26665e400b0c
mainaenock <mainaenock10@gmail.com>
2024-05-08 04:20:24 -0400

    inheritance 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+"""
+empty class
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name: str, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    child class
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+
+    def area(self):
+        """
+        calculates the area
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        prints
+        """
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+
+class Square(Rectangle):
+    """
+    grandchild class
+    """
+    def __init__(self, size):
+        self.__size = size
+        super().__init__(size, size)
+        self.integer_validator("size", self.__size)
