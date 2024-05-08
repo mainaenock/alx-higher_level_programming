aa7eb7a7560349ba53eb087d74658cf0cb4fd124
mainaenock <mainaenock10@gmail.com>
2024-05-08 10:33:08 -0400

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename=""):
     """
     with open(filename, "r", encoding="utf-8") as file:
         for line in file:
-            print("{}".format(line.strip()))
+            print("{}".format(line.strip(), end=""))
