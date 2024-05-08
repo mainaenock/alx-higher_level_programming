f855fbabc178c7349fd6d552a36e999a000a6140
mainaenock <mainaenock10@gmail.com>
2024-05-08 13:17:56 -0400

    dump

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+save to json
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+     function that creates an Object from a “JSON file”
+     """
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
