d0881955a8284a736257bf18d38806b048706561
mainaenock <mainaenock10@gmail.com>
2024-05-13 15:21:18 -0400

    almost a circle 7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -103,6 +103,9 @@ class Rectangle(Base):
             self.id, self.__x, self.__y, self.__width, self.__height))
 
     def update(self, *args):
+        """
+        assigns an argument to each attribute
+        """
         if len(args) >= 1:
             self.id = args[0]
         if len(args) >= 2:
