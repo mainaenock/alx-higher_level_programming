f681c5aa0677960971a2e0bbe83b8c93f7d68f64
mainaenock <mainaenock10@gmail.com>
2024-05-14 06:15:38 -0400

    almost a circle 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -50,9 +50,9 @@ class Square(Rectangle):
             if len(args) >= 2:
                 self.size = args[1]
             if len(args) >= 3:
-                self.__x = args[2]
+                self.x = args[2]
             if len(args) >= 4:
-                self.__y = args[3]
+                self.y = args[3]
 
         else:
             for key, value in kwargs.items():
