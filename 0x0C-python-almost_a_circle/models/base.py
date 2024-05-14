9c1a19d7ae6754f7e9d3377405a5c75aa2c5ec5b
mainaenock <mainaenock10@gmail.com>
2024-05-14 06:52:46 -0400

    almost a circle 14

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ base class
 """
 
 
+import json
+
+
 class Base:
     """
     new class
@@ -17,3 +20,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of list_dictionaries:
+        """
+        if list_dictionaries is None:
+            return "[]"
+
+        else:
+            return (json.dumps(list_dictionaries))
