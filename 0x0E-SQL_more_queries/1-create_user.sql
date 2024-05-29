b3e85eac456edfd790b1b0c0fe05a524632e7ce4
mainaenock <mainaenock10.com>
2024-05-29 06:35:10 -0400

    create userr

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,12 @@
 --creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY "user_0d_1_pwd";
+CREATE USER
+IF NOT EXISTS user_0d_1@localhost
+	IDENTIFIED BY 'user_0d_1_pwd';
 
 -- privilages
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES
+ON *.*
+TO 'user_0d_1'@'localhost';
 
 -- apply the privileges
 FLUSH PRIVILEGES;
