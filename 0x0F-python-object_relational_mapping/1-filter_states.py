b1046b56f13c19cd262019da475806d453220a1e
mainaenock <mainaenock10.com>
2024-06-25 02:47:00 -0400

    ORM 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""  lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa """
+"""  lists all states with a name starting with
+    N (upper N) from the database hbtn_0e_0_usa """
 
 if __name__ == '__main__':
 
@@ -17,7 +18,8 @@ if __name__ == '__main__':
                 )
 
         cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%'"
+                    "ORDER BY states.id ASC;")
         results = cur.fetchall()
 
         for row in results:
